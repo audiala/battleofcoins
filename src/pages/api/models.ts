35842a762267a1b8a2727f68be9fd67d72b7c6f7
Guillaume Pernelle <gpernelle@gmail.com>
2024-11-27 11:40:24 +0100

    netlify (#2)

--- src/pages/api/models.ts
+++ src/pages/api/models.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from 'astro';
 import axios from 'axios';
 
-export const get: APIRoute = async () => {
+export const GET: APIRoute = async () => {
   try {
     const response = await axios.get('https://nano-gpt.com/api/models');
     return new Response(JSON.stringify(response.data), {
